f491546c51e18016f70e9e92959bfb599ecac2c4
Zack H Jali <tschiff6577@gmail.com>
2025-08-23 00:52:10 +0100

    changes in .gitignore

--- obj/Debug/x32/sample/Account_Database.d
+++
@@ -1,7 +0,0 @@
-obj/Debug/x32/sample/Account_Database.o: src/Account/Database.cpp \
-  src/Account/../../core/Account/Database.h \
-  src/Account/../../core/Account/../../include/lfile-ini/iem-ini.h \
-  src/Account/../../core/Account/Data.h
-src/Account/../../core/Account/Database.h:
-src/Account/../../core/Account/../../include/lfile-ini/iem-ini.h:
-src/Account/../../core/Account/Data.h:
